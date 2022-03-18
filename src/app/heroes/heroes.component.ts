53cac5ccef474a7dafb4bc6b23ebdd72e6b42689
Douglas Resende <douglasmresende@gmail.com>
2022-03-18 14:59:23 -0300

    created services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,30 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 import { HEROES } from '../mock-heroes';
+//import { HEROES } from '../mock-heroes';
 
 @Component({
-  selector: 'app-heroes', //o seletor de elementos CSS do componente
+  selector: 'app-heroes',
   templateUrl: './heroes.component.html', //o local do arquivo de modelo do componente.
   styleUrls: ['./heroes.component.css'] //a localização dos estilos CSS privados do componente.
 })
 
 export class HeroesComponent implements OnInit {
+  //injetando  HeroService parâmetro privado;
+  constructor(private heroService: HeroService) { }
 
-  heroes = HEROES;
+  //inicializa
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
-  selectedHero?: Hero;
+  heroes: Hero[] = [];
+  selectedHero: Hero | undefined;
 
-  constructor() {}
-  ngOnInit(): void {}
+  //metodo para recuperar os herois, emulando a chama com a lista do mock com o bservável
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => {
+        // AQUI DENTRO FAZ DEPOIS QUE A CHAMADA ACABA
+        this.heroes = heroes;
+      });
+  }
+
+  /* original
+  getHeroes(); void {
+    this.heroes = this.heroService.getHeroes();
+  }
+  */
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-
-
-
-
-
 }
 
 
